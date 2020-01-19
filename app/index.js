452dfe8f56af048ffd70fbcdca4317a4acff7c57
adam13ah <adam.hagen@gmail.com>
2020-01-18 19:53:46 -0500

    Initial code smashing

--- app/index.js
+++ app/index.js
@@ -9,6 +9,10 @@ import { me as device } from "device";
 import { today, goals } from "user-activity";
 import { preferences, units } from "user-settings";
 
+import * as appointment from "./appointment";
+import * as clock from "./clock";
+import { fromEpochSec, timeUntil, trimString } from "../common/utils";
+
 const heartImage = document.getElementById("hrm-img");
 const separator = document.getElementById("separator");
 const deviceType = device.modelName;
@@ -163,6 +167,35 @@ function processWeatherData(data) {
       weatherImage.href = 'assets/weather/mist.png';
       weatherImage.style.fill = 'white';
       break;
+  }
+}
+
+//************** Calendar  ******************************/
+const time = document.getElementById("time");
+const title = document.getElementById("title");
+const details = document.getElementById("details");
+
+clock.initialize("minutes", data => {
+  // Clock ticked, update UI
+  time.text = data.time;
+  renderAppointment();
+});
+
+appointment.initialize(() => {
+  // We have fresh calendar data
+  clock.tick();
+});
+
+function renderAppointment() {
+  let event = appointment.next();
+  if (event) {
+    title.text = event.title.substr(0, 20);
+    details.text = `${timeUntil(fromEpochSec(event.startDate))} ${
+      event.location ? `@ ${event.location}` : ""
+    }`;
+  } else {
+    title.text = "No appointments";
+    details.text = "";
   }
 }
 
